e4b1a2b8479dcf458b82b90e3f298094cfbc335c
Aleesha Shaji <aleeshaalan@gmail.com>
2019-03-14 12:46:19 +0530

    first basic version,with owned contract

---
+++ Owned.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.11;
+
+contract Owned {
+  // State variables
+  address owner;
+
+  //modifiers
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  // constructor
+  function  Owned () public {
+    owner = msg.sender;
+  }
+}
+
